0ddb61f2d2ce60c1f26c4eb5fbb3af3a7707fa89
IagoCarrera <carlos_ic_silva@estudante.sesisenai.org.br>
2026-05-11 19:42:36 -0300

    Criação do projeto
    Criação somente do projeto vazio com spring initialize

---
+++ satestedesistemas/src/main/java/com/tecdes/satestedesistemas/SatestedesistemasApplication.java
@@ -0,0 +1,13 @@
+package com.tecdes.satestedesistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatestedesistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatestedesistemasApplication.class, args);
+	}
+
+}
---
+++ satestedesistemas/src/test/java/com/tecdes/satestedesistemas/SatestedesistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdes.satestedesistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatestedesistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
